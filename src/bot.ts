633f927ee965828bb4f45ecf2c61b962de4fdb1e
Alex Chesnay <me@alex-chesnay.fr>
2019-09-19 01:42:22 +0200

    feat: message de bienvenu

--- src/bot.ts
+++ src/bot.ts
@@ -1,4 +1,4 @@
-import { Client, Message } from 'discord.js';
+import { Client, Message, GuildMember, GuildChannel, TextChannel } from 'discord.js';
 
 import 'dotenv/config';
 import Command from './command';
@@ -20,6 +20,15 @@ export default class Bot {
         this.bot.on('ready', () => {
             // this.bot.user.setAvatar('assets/avatar.png'); // Décommenter cette ligne si vous souhaitez mettre un avatar au bot
             this.bot.user.setActivity('Être à votre service.');
+        })
+
+        // S'exécute lorsqu'un utilisateur rejoint le server pour la première fois
+        this.bot.on('guildMemberAdd', (member: GuildMember) => {
+            const channel: GuildChannel | undefined = member.guild.channels.find(ch => ch.name === 'home' || ch.name === 'accueil');
+            if (!channel || channel.type !== 'text') return;
+
+            const channelText = <TextChannel>channel;
+            channelText.send(`Bienvenue, ${member}`);
         })
 
         // Intercepte les messages, vérifie si c'est une commande, si c'est le cas, la classe Command s'occupe de les parser
