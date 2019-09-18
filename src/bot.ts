a645dbbed01588cdffed7c838731ef4db46522f6
Alex Chesnay <me@alex-chesnay.fr>
2019-09-18 23:53:48 +0200

    feat: ajout d'une activité et d'un avatar au bot

--- src/bot.ts
+++ src/bot.ts
@@ -16,6 +16,12 @@ export default class Bot {
      * @return void
      */
     public start(): void {
+        // S'exécute lorsque le bot est prêt
+        this.bot.on('ready', () => {
+            // this.bot.user.setAvatar('assets/avatar.png'); // Décommenter cette ligne si vous souhaitez mettre un avatar au bot
+            this.bot.user.setActivity('Être à votre service.');
+        })
+
         // Intercepte les messages, vérifie si c'est une commande, si c'est le cas, la classe Command s'occupe de les parser
         this.bot.on('message', (message: Message) => {
             if (!message.content.startsWith('!')) return;
